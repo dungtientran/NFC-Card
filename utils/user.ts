b3bbb41413f5a5dec055033ada85d9633bb371ad
dungtientran <dungcq86@gmail.com>
2023-09-18 11:03:13 +0700

    fix

--- utils/user.ts
+++ utils/user.ts
@@ -2,7 +2,7 @@ export const user = {
     name: 'Mrs. Thanh Uyên',
     position: "Sales Manager",
     phone: "0933945236",
-    messages_fb: "thanh.uyen.0402",
+    messages_fb: "100000333770126",
     zalo: '84933945236',
     email: "miurey.nguyen@metasolutions.software",
     whatsapp: "84933945236",
